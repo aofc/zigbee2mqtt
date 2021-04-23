d39b160ead3d2e54a935fa991769716b9e546f9a
aofc <robin.sylvestre@polymtl.ca>
2021-04-23 15:48:05 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,12 +13,24 @@ sudo systemctl stop zigbee2mqtt
 echo "Creating backup of configuration..."
 cp -R data data-backup
 
+echo "Removing symbolic link to zigbee-herdsman-converters.aofc..."
+rm -f ./node_modules/zigbee-herdsman-converters
+
 echo "Updating..."
 git checkout HEAD -- npm-shrinkwrap.json
 git pull
 
 echo "Installing dependencies..."
 npm ci
+
+echo "Updating zigbee-herdsman-converters..."
+mv ./node_modules/zigbee-herdsman-converters ./node_modules/zigbee-herdsman-converters.Koenkk
+#git checkout HEAD -- npm-shrinkwrap.json
+#rm -rf ./node_modules/zigbee-herdsman-converters
+cd ../zigbee-herdsman-converters.aofc/
+git pull
+cd ../zigbee2mqtt/
+ln -s -T /opt/zigbee-herdsman-converters.aofc /opt/zigbee2mqtt/node_modules/zigbee-herdsman-converters
 
 echo "Restore configuration..."
 cp -R data-backup/* data
